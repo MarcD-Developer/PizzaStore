ad07c084625901b8e348b94bf76586bfec16f6e4
Marc Dilinila <msdilinila@gmail.com>
2020-09-17 04:23:30 -0400

    Added the entity of entity framework.  Was able to get an api layer working on the front end.

---
+++ PizzaStore_Data/PizzaStore_DataLayer/PizzaList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore_DataLayer
+{
+    public class PizzaList
+    {
+        public IEnumerable<Pizza> getAllPizzas()
+        {
+            var ctx = new PizzaStoreEntities();
+
+            var pizzas = from p in ctx.Pizzas
+                         select p;
+
+            return pizzas;
+        }
+    }
+}
---
+++ PizzaStore_Data/PizzaStore_WebLayer/Controllers/PizzaController.cs
@@ -0,0 +1,42 @@
+﻿using PizzaStore_DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PizzaStore_WebLayer.Controllers
+{
+    public class PizzaController : ApiController
+    {
+        // GET: api/Pizza
+        public IEnumerable<Pizza> Get()
+        {
+            var pizzaDB = new PizzaList();
+            
+            return pizzaDB.getAllPizzas();
+        }
+
+        // GET: api/Pizza/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //// POST: api/Pizza
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Pizza/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Pizza/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
