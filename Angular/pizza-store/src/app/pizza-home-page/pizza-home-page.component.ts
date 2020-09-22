febae92edc335a244c7ccc18a239589e9386367b
Marc Dilinila <msdilinila@gmail.com>
2020-09-21 22:43:02 -0400

    Created first page of application

---
+++ Angular/pizza-store/src/app/pizza-home-page/pizza-home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-home-page',
+  templateUrl: './pizza-home-page.component.html',
+  styleUrls: ['./pizza-home-page.component.css']
+})
+export class PizzaHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
