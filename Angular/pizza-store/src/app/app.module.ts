7c1b79b19a1fe8509f7391e04911a6971fc9a284
SmokedEel <marc.dilinila@gmail.com>
2020-09-26 22:16:00 -0400

    Created pizza home page module

--- Angular/pizza-store/src/app/app.module.ts
+++ Angular/pizza-store/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaHomePageComponent } from './pizza-home-page/pizza-home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaHomePageComponent
   ],
   imports: [
     BrowserModule,
